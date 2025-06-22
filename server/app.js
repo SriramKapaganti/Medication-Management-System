2e5825e27d52a21a90eba43dcc9add0cdae9920b
Sriram Kapaganti <koushiksriram3967@gmail.com>
2025-06-22 11:42:20 +0530

    updated origin in app.js

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:3000',  
+  origin: 'https://medication-management-system-nu.vercel.app/',  
   credentials: true                 
 }));
 app.use('/api/auth', authRoutes);
